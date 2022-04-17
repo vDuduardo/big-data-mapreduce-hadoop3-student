08daab15f8aa71b18ccf100d694dc4c40d770aec
vDuduardo <everbinen@gmail.com>
2022-04-16 22:20:21 -0300

    Commiting until exercise 5 of TDE01

--- src/advanced/customwritable/AverageTemperature.java
+++ src/advanced/customwritable/AverageTemperature.java
@@ -2,10 +2,7 @@ package advanced.customwritable;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -13,6 +10,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.log4j.BasicConfigurator;
+import sun.print.DialogOwner;
 
 import java.io.IOException;
 
@@ -32,6 +30,25 @@ public class AverageTemperature {
         // criacao do job e seu nome
         Job j = new Job(c, "media");
 
+        //Registro de classes
+        j.setJarByClass(AverageTemperature.class);
+        j.setMapperClass(MapForAverage.class);
+        j.setReducerClass(ReduceForAverage.class);
+        j.setCombinerClass(CombineForAverage.class);
+
+        //Definição de tipos de saide
+        //Map (Text, FireAvgTemperature)
+        j.setMapOutputKeyClass(Text.class);
+        j.setMapOutputValueClass(FireAvgTempWritable.class);
+
+        //reduce (Text, DoubleWritable
+        j.setOutputKeyClass(Text.class);
+        j.setOutputValueClass(DoubleWritable.class);
+
+        //Definição de arquivos de entrada e saida
+        FileInputFormat.addInputPath(j, input);
+        FileOutputFormat.setOutputPath(j, output);
+
         // lanca o job e aguarda sua execucao
         System.exit(j.waitForCompletion(true) ? 0 : 1);
     }
@@ -43,14 +60,56 @@ public class AverageTemperature {
         public void map(LongWritable key, Text value, Context con)
                 throws IOException, InterruptedException {
 
+            //Obtendo a linha para processamento
+            String linha = value.toString();
+
+            //Quebrando em campos
+            String campos[] = linha.split(",");
+
+            //Obtendo a temperatura
+            double temperatura = Double.parseDouble(campos[8]);
+
+            //Obtendo mes
+            String mes = campos[2];
+
+            //Emitir (chave, valor) -> ("media", (n=1, sum=temperatura)
+            con.write(new Text("media"), new FireAvgTempWritable(1, temperatura));
+            con.write(new Text(mes), new FireAvgTempWritable(1, temperatura));
         }
     }
 
-    public static class ReduceForAverage extends Reducer<Text, FireAvgTempWritable, Text, FloatWritable> {
+    public static class CombineForAverage extends Reducer<Text, FireAvgTempWritable, Text, FireAvgTempWritable>{
+
+        public void reduce(Text key, Iterable<FireAvgTempWritable> values, Context con)
+                throws IOException, InterruptedException {
+            //O objetivo desse combine ẽ SOMAR os Ns e as SOMAS parciais
+            int totalN = 0;
+            double totalSoma = 0.0;
+            for(FireAvgTempWritable o : values){
+                totalN += o.getN();
+                totalSoma += o.getSoma();
+            }
+
+            //enviando do combiner para o sort/shuffle
+            con.write(key, new FireAvgTempWritable(totalN, totalSoma));
+        }
+    }
+
+
+    public static class ReduceForAverage extends Reducer<Text, FireAvgTempWritable, Text, DoubleWritable> {
         public void reduce(Text key, Iterable<FireAvgTempWritable> values, Context con)
                 throws IOException, InterruptedException {
 
+            int nTotal = 0;
+            double somaTotal = 0.0;
+            for(FireAvgTempWritable o : values){
+                nTotal += o.getN();
+                somaTotal += o.getSoma();
+            }
+
+            double media = somaTotal/ nTotal;
+
+            con.write(key, new DoubleWritable(media));
         }
     }
-
 }
--- src/advanced/customwritable/ForestFireWritable.java
+++ src/advanced/customwritable/ForestFireWritable.java
@@ -1,12 +1,30 @@
 package advanced.customwritable;
 
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class ForestFireWritable {
+public class ForestFireWritable implements WritableComparable<ForestFireWritable> {
+
+    private double temp;
+    private double wind;
 
 
+    @Override
+    public int compareTo(ForestFireWritable forestFireWritable) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
